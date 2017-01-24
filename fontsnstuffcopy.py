0aa90f096aeefd47e9f1d7d65e6369dcc8fde76e
Memex11 <dax.mahoney@gmail.com>
2017-01-24 02:34:41 +0000

    added a class for the pictures

--- fontsnstuffcopy.py
+++ fontsnstuffcopy.py
@@ -5,6 +5,9 @@ from pygame.locals import *
 
 pygame.init()
 
+suspectlist = ['Eloy.png','Gray.png','Binod.png','Kraus.png']
+
+
 FPS = 30 #frames per second setting
 fpsClock = pygame.time.Clock()
 
@@ -64,7 +67,14 @@ def counterwise(input_x,input_y,input_direction):
 
     return new_x,new_y,new_direction
 
+class Decisio_Picture_Deal:
+    def __init__(self,x_part,y_part,direction_part,image_part):
+        self.x = x_part
+        self.y = y_part
+        self.direction = direction_part
+        self.Img = pygame.image.load(image_part)
 
+        
 #window set up
 DISPLAYSURF = pygame.display.set_mode((600,800))
 pygame.display.set_caption('fontsnstuff')
@@ -76,24 +86,18 @@ RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,128)
 
-eloyImg = pygame.image.load('/home/pi/Desktop/Eloy.png')
-eloy_x = 10
-eloy_y =10
-eloy_direction = 'right'
+#creates people objects
+eloy = Decisio_Picture_Deal(10,10,'right','Eloy.png')
+gray = Decisio_Picture_Deal(100,50,'left','Gray.png')
 
-
-grayImg = pygame.image.load('/home/pi/Desktop/Gray.png')
-gray_x = 100
-gray_y = 50
-gray_direction = 'left'
 
 fontObj = pygame.font.Font('freesansbold.ttf',32)
 
-text_GraySurfaceObj = fontObj.render('Gray X:{0} Y:{1}'.format(gray_x,gray_y),True,GREEN,BLUE)
+text_GraySurfaceObj = fontObj.render('Gray X:{0} Y:{1}'.format(gray.x,gray.y),True,GREEN,BLUE)
 text_GrayRectObj = text_GraySurfaceObj.get_rect()
 text_GrayRectObj.center =(200,300)
 
-text_EloySurfaceObj = fontObj.render('Eloy X:{0} Y:{1}'.format(eloy_x,eloy_y),True,GREEN,BLUE)
+text_EloySurfaceObj = fontObj.render('Eloy X:{0} Y:{1}'.format(eloy.x,eloy.y),True,GREEN,BLUE)
 text_EloyRectObj = text_EloySurfaceObj.get_rect()
 text_EloyRectObj.center =(200,150)
 
@@ -103,18 +107,16 @@ while True: #main game loop
 
     DISPLAYSURF.blit(text_GraySurfaceObj,text_GrayRectObj)
     DISPLAYSURF.blit(text_EloySurfaceObj,text_EloyRectObj)
-    
-  # I see that some sort of class object might be the next step
-  
-    eloy_x,eloy_y,eloy_direction = clockwise(eloy_x,eloy_y,eloy_direction )
-    gray_x,gray_y,gray_direction = counterwise(gray_x,gray_y,gray_direction )
+      
+    eloy.x,eloy.y,eloy.direction = clockwise(eloy.x,eloy.y,eloy.direction )
+    gray.x,gray.y,gray.direction = counterwise(gray.x,gray.y,gray.direction )
 
 
-    DISPLAYSURF.blit(eloyImg,(eloy_x,eloy_y))
-    DISPLAYSURF.blit(grayImg,(gray_x,gray_y))
+    DISPLAYSURF.blit(eloy.Img,(eloy.x,eloy.y))
+    DISPLAYSURF.blit(gray.Img,(gray.x,gray.y))
     
-    text_EloySurfaceObj = fontObj.render('Eloy X:{0} Y:{1}'.format(eloy_x,eloy_y),True,GREEN,BLUE)
-    text_GraySurfaceObj = fontObj.render('Gray X:{0} Y:{1}'.format(gray_x,gray_y),True,GREEN,BLUE)
+    text_EloySurfaceObj = fontObj.render('Eloy X:{0} Y:{1}'.format(eloy.x,eloy.y),True,GREEN,BLUE)
+    text_GraySurfaceObj = fontObj.render('Gray X:{0} Y:{1}'.format(gray.x,gray.y),True,GREEN,BLUE)
 
     DISPLAYSURF.blit(text_GraySurfaceObj,text_GrayRectObj)
     DISPLAYSURF.blit(text_EloySurfaceObj,text_EloyRectObj)
